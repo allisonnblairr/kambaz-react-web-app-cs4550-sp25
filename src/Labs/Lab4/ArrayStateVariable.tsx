e3f19499af5b3b19de8a36805219f28342bfd69e
allisonnblairr <allyyblairr@gmail.com>
2025-02-26 12:59:27 -0500

    up to 115 on labs

---
+++ src/Labs/Lab4/ArrayStateVariable.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+export default function ArrayStateVariable() {
+  const [array, setArray] = useState([1, 2, 3, 4, 5]);
+  const addElement = () => {
+    setArray([...array, Math.floor(Math.random() * 100)]);
+  };
+  const deleteElement = (index: number) => {
+    setArray(array.filter((item, i) => i !== index));
+  };
+  return (
+    <div id="wd-array-state-variables">
+      <h2>Array State Variable</h2>
+      <button onClick={addElement} className="btn btn-success">Add Element</button>
+      <ul style={{ listStyleType: "none", padding: 0 }}>
+        {array.map((item, index) => (
+          <li key={index}>
+          <div
+            style={{
+              display: "flex",
+              justifyContent: "space-between",
+              border: "1px solid #ccc",
+              padding: "3px",
+              borderRadius: "5px",
+            }}
+          >
+            {item}
+            <button
+              onClick={() => deleteElement(index)}
+              id="wd-delete-element-click"
+              className="btn btn-danger"
+            >
+              Delete
+            </button>
+          </div>
+        </li>
+        ))}
+      </ul>
+      <hr/>
+    </div>
+  );
+}
---
+++ src/Labs/Lab4/ReduxExamples.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import HelloRedux from "./ReduxExamples/HelloRedux";
+import CounterRedux from "./ReduxExamples/CounterRedux";
+import AddRedux from "./ReduxExamples/AddRedux";
+import TodoList from "./ReduxExamples/todos/TodoList";
+
+export default function ReduxExamples() {
+  return(
+    <div>
+      <h2>Redux Examples</h2>
+      <HelloRedux />
+      <CounterRedux />
+      <AddRedux />
+      <TodoList />
+    </div>
+  );
+};
